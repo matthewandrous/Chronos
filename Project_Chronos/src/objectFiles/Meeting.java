380f9e4f3b1062d3ee2f4c87fb265f147dfeaa8b
Muyao Xu <muyaoxu@usc.edu>
2017-11-11 16:19:08 -0800

    add getters and setters for Meeting

--- Project_Chronos/src/objectFiles/Meeting.java
+++ Project_Chronos/src/objectFiles/Meeting.java
@@ -5,13 +5,14 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Meeting {
-	private int eventID;
+	private int meetingID;
 	private Date startDate;
 	// 24 hour clock
 	private int startTime;
 	private int numDays;
 	// number of meeting options per day
 	private int numMeetingsPerDay;
+	// col: numDays row: numMeetingsPerDay
 	private ArrayList<ArrayList<Availability>> timetable;
 	private ArrayList<User> usersAnswered;
 	private int numUsers;
@@ -20,6 +21,21 @@ public class Meeting {
 		
 	}
 	
+	public void setNumUsers(int n) {
+		numUsers = n;
+	}
+	
+	public void setNumDays(int n) {
+		numDays = n;
+	}
+	
+	public void setNumMeetingsPerDay(int n) {
+		numMeetingsPerDay = n;
+	}
+	
+	public int getMeetingID() {
+		return meetingID;
+	}
 	public int getNumUsersAvailable(int row, int col) {
 		Availability curr_ava = timetable.get(row).get(col);
 		return curr_ava.getNumAvailableUsers();

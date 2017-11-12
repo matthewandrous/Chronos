a2bf9c9c514b7b7f907fadc98593094af819a341
Muyao Xu <muyaoxu@usc.edu>
2017-11-11 16:02:20 -0800

    Revert "add implementations for Meeting Availability"
    This reverts commit 58432b271a4c04fa009d92664511cae12428af95.
    
    test

--- Project_Chronos/src/objectFiles/Meeting.java
+++ Project_Chronos/src/objectFiles/Meeting.java
@@ -1,34 +1,26 @@
 package objectFiles;
 
 import java.sql.Time;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class Meeting {
 	private int eventID;
 	private Date startDate;
-	// 24 hour clock
-	private int startTime;
-	private int numDays;
-	// number of meeting options per day
-	private int numMeetingsPerDay;
-	private ArrayList<ArrayList<Availability>> timetable;
-	private ArrayList<User> usersAnswered;
-	private int numUsers;
+	private Time startTime;
 	
-	public Meeting() {
-		
-	}
 	
-	public int getNumUsersAvailable(int row, int col) {
-		Availability curr_ava = timetable.get(row).get(col);
-		return curr_ava.getNumAvailableUsers();
-	}
 	
-	public void markTimetable(int row, int col, Boolean isAvailable, User user) {
-		Availability curr_ava = timetable.get(row).get(col);
-		curr_ava.setAvailability(user, isAvailable);
-	}
 	
+	
+	
+/*String eventID
+String timeZone
+Int meetingPeriod
+String startDate
+String endDate
+String startTime
+String endTime
+ArrayList<ArrayList<TimeSlot>> timeTable
+ArrayList<User> usersAnswered*/
 
 }
